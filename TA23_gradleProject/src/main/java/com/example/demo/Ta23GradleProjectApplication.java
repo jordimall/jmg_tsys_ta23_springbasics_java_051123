966f4e788b68025d0c35195d403d1225710a56f8
jordimall <jordi.mallafre@hotmail.com>
2023-11-05 19:40:29 +0100

    proyecto gradle

---
+++ TA23_gradleProject/src/main/java/com/example/demo/Ta23GradleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta23GradleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta23GradleProjectApplication.class, args);
+	}
+
+}
