70eda290b1d9bd54f7daa0bcf017a0840b0dfc1b
jordimall <jordi.mallafre@hotmail.com>
2023-11-05 19:37:22 +0100

    Proyecto maven

---
+++ TA23_mavenProject/src/main/java/com/example/demo/Ta23MavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta23MavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta23MavenProjectApplication.class, args);
+	}
+
+}
